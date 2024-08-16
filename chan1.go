8212717fb1a33fc859cf203277d5f518e2d63559
Sergey <sgudkovv@gmail.com>
2024-08-16 16:23:39 +0500

    Init

---
+++ chan1.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"hash/fnv"
+	"time"
+)
+
+func main() {
+
+	chan1 := make(chan uint32, 1)
+
+	//Run in a goroutine to calculate the hash
+	//and write to the channel
+	go runchannels(chan1)
+
+	//Read from the channel and print the results123
+	go getNumber(chan1)
+
+	time.Sleep(2 * time.Second)
+
+}
+
+func runchannels(in chan<- uint32) {
+	for i := 0; i < 500; i++ {
+		val := i
+		in <- hash(fmt.Sprint(val))
+		fmt.Println("Write to chan2", val)
+	}
+
+	close(in)
+}
+
+func getNumber(in <-chan uint32) {
+	for val := range in {
+		fmt.Println("get from chan1", val)
+	}
+
+}
+
+func hash(s string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return h.Sum32()
+}
